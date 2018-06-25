843d1be18d1e8e0f3fa9b29a8562c1e33e8cbc7c
janzse <sebastian.janzen@fh-bielefeld.de>
2018-06-25 11:24:34 +0200

    refractoring

--- server.js
+++ server.js
@@ -1,18 +1,39 @@
-const app = require('express')();
-const server = app.listen('4811');
-const io = require('socket.io').listen(server);
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+server.listen(4811);
+
+app.get('/', function (req, res) {
+    res.sendfile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket) {
+    socket.emit('news', { hello: 'world' });
+    socket.on('my other event', function (data) {
+        console.log(data);
+    });
+});
 
 /*
 app.get('/', function (req, res) {
     res.sendFile('./index.js', {root: __dirname});
 });
 */
+ // Make sure this returns the socketio
 
-app.use('./routes', require('./routes'));
+//app.set('socketIo', io);
+
+//app.use('./', require('./routes'));
+
+app.route('/test').get(function (req, res) {
+    let socket = req.app.get('socketIo');
+    socket.emit('hello', 'world');
+    res.end();
+});
 
 io.sockets.on('connection', function(socket){
     socket.emit('test', 1);
-})
+});
 
 module.exports = app;
 
