a1c2232ba5ba6a239e716acb5997f25db5039ffb
janzse <sebastian.janzen@fh-bielefeld.de>
2018-06-25 09:52:52 +0200

    index

--- server.js
+++ server.js
@@ -3,5 +3,15 @@ const server = app.listen('4811');
 const io = require('socket.io').listen(server);
 
 app.get('/', function (req, res) {
-    res.sendfile('./index.html' );
+    res.sendFile('./index.html', {root: __dirname});
 });
+
+let routesApi = require('./index');
+app.use('/api', routesApi);
+module.exports = app;
+
+io.sockets.on('connection', function(socket){
+    socket.emit('test', 1);
+})
+
+console.log("HTTP-Server is running on", "http://localhost:4811");
