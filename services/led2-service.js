7139213e96117e1c0a831bbef39900ecd24f10ae
janzse <sebastian.janzen@fh-bielefeld.de>
2018-06-28 14:24:41 +0200

    led2 service

--- services/led2-service.js
+++ services/led2-service.js
@@ -12,13 +12,39 @@ const led2Publisher = new cote.Publisher({
     respondsTo: ['led2']
 });
 
+const humid1Subscriber = new cote.Subscriber({
+    name: 'humidity 1 subscriber',
+    namespace: 'sensor',
+    subscribesTo: ['bright2']
+})
+
+let led_status;
+
 led2Responder.on('led2', (request, callback) => {
-    callback("on");
+    if (brightness !== null)
+    {
+        led_status = {
+            description: 'Light status changed',
+            value: true,
+            timestamp: new Date()
+        };
+        callback(led_status);
+    }
+    else
+        callback("No brightness value");
 });
+
+let humidity;
+
+humid1Subscriber.on('bright2', (request) => {
+    if (request !== null && request !== humidity)
+        humidity = request;
+});
+
 
 function updateLed2()
 {
-    led2Publisher.publish('led2', "on");
+    led2Publisher.publish('led2', led_status);
 }
 
 setInterval(updateLed2, 1000);
