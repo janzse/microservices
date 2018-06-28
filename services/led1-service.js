dfa5281bf587bd03a155449922a5e67396518ba8
janzse <sebastian.janzen@fh-bielefeld.de>
2018-06-28 21:30:03 +0200

    update

--- services/led1-service.js
+++ services/led1-service.js
@@ -12,18 +12,6 @@ const led1Publisher = new cote.Publisher({
     broadcasts: ['led1-changed']
 });
 
-const led1DebugResponder = new cote.Responder({
-    name: 'led 1 responder',
-    namespace: 'sensor',
-    respondsTo: ['led1-debug']
-});
-
-const led1DebugPublisher = new cote.Publisher({
-    name: 'led 1 publisher',
-    namespace: 'sensor',
-    broadcasts: ['led1-debug']
-});
-
 const bright2Subscriber = new cote.Subscriber({
     name: 'brightness 2 subscriber',
     namespace: 'sensor',
@@ -44,18 +32,6 @@ led1Responder.on('led1-set', (request, callback) => {
     callback(led_status);
 });
 
-let debug;
-
-led1DebugResponder.on('led1-debug', (request, callback) => {
-    debug = {
-        description: 'Light status 1 changed',
-        value: false,
-        timestamp: new Date()
-    };
-    led1DebugPublisher.publish('led1-debug', request);
-    callback(request);
-});
-
 let brightness;
 
 bright2Subscriber.on('bright2', (request) => {
@@ -70,8 +46,8 @@ function publishLed1Status()
         value: state,
         timestamp: new Date()
     };
-    //dummyVal = !dummyVal;
-    led1Publisher.publish('led1-changed', led_status);
+    dummyVal = !dummyVal;
+    led1Publisher.publish('led1-changed', dummyVal);
 }
 
 setInterval(publishLed1Status, 1000);
