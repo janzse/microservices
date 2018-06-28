b3c898faf43d82cae04e668a9e73a81d837e06d0
janzse <sebastian.janzen@fh-bielefeld.de>
2018-06-28 14:18:26 +0200

    update

--- services/led1-service.js
+++ services/led1-service.js
@@ -26,7 +26,7 @@ led1Responder.on('led1', (request, callback) => {
     {
         led_status = {
             description: 'Light status changed',
-            value: brightness,
+            value: true,
             timestamp: new Date()
         };
         callback(led_status);
@@ -37,18 +37,15 @@ led1Responder.on('led1', (request, callback) => {
 
 let brightness;
 
-//setInterval(() => {
-    bright2Subscriber.on('bright2', (request) => {
-        if (request !== null && request !== brightness)
-            brightness = request;
-    });
-//}, 1000);
+bright2Subscriber.on('bright2', (request) => {
+    if (request !== null && request !== brightness)
+        brightness = request;
+});
 
 
 function publishLed1Status()
 {
-    if (brightness !== null || brightness !== undefined)
-        led1Publisher.publish('led1-changed', led_status);
+    led1Publisher.publish('led1-changed', led_status);
 }
 
 setInterval(publishLed1Status, 1000);
