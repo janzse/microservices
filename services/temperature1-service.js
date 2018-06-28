e89952156226476621bb6fdb44a4880a0fffc6b1
janzse <sebastian.janzen@fh-bielefeld.de>
2018-06-29 00:13:54 +0200

    update

--- services/temperature1-service.js
+++ services/temperature1-service.js
@@ -14,15 +14,14 @@ const temp1Publisher = new cote.Publisher({
 
 let temp_status;
 
-temp1Responder.on('temp1-data', (request, callback) => {
+temp1Responder.on('temp1-data', (request) => {
     //let temperature = Math.floor((Math.random() * 40) + Math.random() * 10);
     temp_status = {
         description: 'Temperature 1',
         value: request.value,
         timestamp: new Date()
     };
-    //callback(temp_status);
-    temp1Publisher.publish('temp1-data', temp_status);
+    //temp1Publisher.publish('temp1-data', temp_status);
 });
 
 function publishTemperature1()
