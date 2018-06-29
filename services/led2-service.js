106b589957a2f0e2a22961a89fbaa40b6a48f2a4
janzse <sebastian.janzen@fh-bielefeld.de>
2018-06-29 19:18:43 +0200

    update

--- services/led2-service.js
+++ services/led2-service.js
@@ -32,19 +32,15 @@ led2Responder.on('led2-set', (request) => {
     console.log("changed:",request.value);
     led2Publisher.publish('led2-changed', request.value);
 });
-let oldValue;
+
 brightSubscriber.on('brightness-data', (brightness) => {
     if (brightness.value <= 200) {
-        if (oldValue === undefined)
-            oldValue = brightness.value;
-        if (brightness.value !== oldValue && (oldValue >= 200)) {
-            led_status = {
-                description: 'Light status 2 changed',
-                value: true,
-                timestamp: new Date()
-            };
-            led2Publisher.publish('led2-changed', led_status);
-        }
+        led_status = {
+            description: 'Light status 2 changed',
+            value: true,
+            timestamp: new Date()
+        };
+        led2Publisher.publish('led2-changed', led_status);
     }
 });
 
