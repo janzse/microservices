6f3587ceda36c736c4858a4c819be48345160c59
janzse <sebastian.janzen@fh-bielefeld.de>
2018-06-29 19:48:33 +0200

    alarm

---
+++ services/alarm-service.js
@@ -0,0 +1,28 @@
+const cote = require('cote');
+
+const alarmPublisher = new cote.Publisher({
+    name: 'alarm publisher',
+    namespace: 'alarm',
+    broadcasts: ['alarm']
+});
+
+let temp2Subscriber = new cote.Subscriber({
+    name: 'temperature 2 subscriber',
+    namespace: 'alarm',
+    respondsTo: ['temp2-data']
+});
+
+let state;
+temp2Subscriber.on('temp2-data', (temperature) => {
+    if (temperature.value > 25) {
+        if (state !== temperature.value) {
+            let temp_status = {
+                description: 'Alarm',
+                value: temperature.value,
+                timestamp: new Date()
+            };
+            alarmPublisher.publish('temp2-data', temp_status);
+            state = temperature.value;
+        }
+    }
+});
